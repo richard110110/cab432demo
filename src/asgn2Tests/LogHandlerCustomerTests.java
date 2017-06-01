833ba604dd65074b35e81ec670e8afa18981e8d8
jeonhoseob <jehose@gmail.com>
2017-06-02 02:44:50 +1000

    Add files via upload

--- src/asgn2Tests/LogHandlerCustomerTests.java
+++ src/asgn2Tests/LogHandlerCustomerTests.java
@@ -3,20 +3,13 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
-import javax.lang.model.type.NoType;
-
 import asgn2Exceptions.LogHandlerException;
 import asgn2Restaurant.LogHandler;
 
 import org.junit.Before;
 import org.junit.Test;
-import junit.*;
 import asgn2Exceptions.CustomerException;
 import asgn2Customers.Customer;
-import asgn2Customers.CustomerFactory;
-import asgn2Customers.DriverDeliveryCustomer;
-import asgn2Customers.DroneDeliveryCustomer;
-import asgn2Customers.PickUpCustomer;
 /**
  * A class that tests the methods relating to the creation of Customer objects in the asgn2Restaurant.LogHander class.
  *
@@ -24,10 +17,24 @@ import asgn2Customers.PickUpCustomer;
  */
 public class LogHandlerCustomerTests {
 	// TO DO
+	private String noName;
+	private String longName;
+	private String noType;
+	private String wrongType;
+	private String noMobileNumber;
+	private String noLocationX;
+	private String noLocationY;
 	ArrayList<Customer> customers = new ArrayList<Customer>();
 	@Before
 	public void setUp() throws CustomerException, LogHandlerException{
 		customers = LogHandler.populateCustomerDataset(".//logs/20170101.txt");
+		noName = new String("19:00:00" + "20:00:00" + "" + "0123456789" + "DVC" + "5" + "5" + "PZV" + "1");
+		longName = new String("19:00:00" + "20:00:00" + "Long Test Name" + "0123456789" + "DVC" + "5" + "5" + "PZV" + "1");
+		noType = new String("19:00:00" + "20:00:00" + "Test Name" + "0123456789" + "" + "5" + "5" + "PZV" + "1");
+		wrongType = new String("19:00:00" + "20:00:00" + "Test Name" + "0123456789" + "ABC" + "5" + "5" + "PZV" + "1");
+		noMobileNumber = new String("19:00:00" + "20:00:00" + "Test Name" + "" + "DVC" + "5" + "5" + "PZV" + "1");
+		noLocationX = new String("19:00:00" + "20:00:00" + "Test Name" + "0123456789" + "DVC" + "" + "5" + "PZV" + "1");
+		noLocationY = new String("19:00:00" + "20:00:00" + "Test Name" + "0123456789" + "DVC" + "5" + "" + "PZV" + "1");
 	}
 	
 	@Test
@@ -63,7 +70,42 @@ public class LogHandlerCustomerTests {
 		assertEquals(customers.get(1).getLocationY(), 4);
 		assertEquals(customers.get(1).getMobileNumber(), "0987654321");
 	}
-
+	
+	@Test(expected = LogHandlerException.class)
+	public void testNoName() throws CustomerException, LogHandlerException{
+		LogHandler.createCustomer(noName);
+	}
+	
+	@Test(expected = LogHandlerException.class)
+	public void testLongName() throws CustomerException, LogHandlerException{
+		LogHandler.createCustomer(longName);
+	}
+	
+	@Test(expected = LogHandlerException.class)
+	public void testNoType() throws CustomerException, LogHandlerException{
+		LogHandler.createCustomer(noType);
+	}
+	
+	@Test(expected = LogHandlerException.class)
+	public void testWrongType() throws CustomerException, LogHandlerException{
+		LogHandler.createCustomer(wrongType);
+	}
+	
+	@Test(expected = LogHandlerException.class)
+	public void testNoMobileNumber() throws CustomerException, LogHandlerException{
+		LogHandler.createCustomer(noMobileNumber);
+	}
+	
+	@Test(expected = LogHandlerException.class)
+	public void testNoLocationX() throws CustomerException, LogHandlerException{
+		LogHandler.createCustomer(noLocationX);
+	}
+	
+	@Test(expected = LogHandlerException.class)
+	public void testNoLocationY() throws CustomerException, LogHandlerException{
+		LogHandler.createCustomer(noLocationY);
+	}
+	
 	@Test(expected = LogHandlerException.class)
 	public void testCreateCustomer() throws CustomerException, LogHandlerException{
 		LogHandler.createCustomer(null);
